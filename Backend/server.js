4a612638e86bac70d3f8c896fbbadf5506a6f3a1
keshav Raj <keshavrajacharya109@gmail.com>
2025-10-05 19:09:38 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,10 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: ['https://appointo-eta.vercel.app', 'http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 
 // DB Connection
